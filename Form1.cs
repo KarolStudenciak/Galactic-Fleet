96dd0e7f8ed82c9e14eb7cce82ce897b95d1c5ed
Karol Wasik <wasik.karol99@gmail.com>
2020-12-05 14:40:01 +0100

    test2

--- Form1.cs
+++ Form1.cs
@@ -25,10 +25,7 @@ namespace GwiezdnaFlota
             Graphics = GamePanel.CreateGraphics();
             Pen = new Pen(Color.Red, 3);
         }
-        public void test()
-        {
 
-        }
         public void Game()
         {
             while (GameStatus.game)
