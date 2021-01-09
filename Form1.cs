0a35fcf7eb1fd55e747af40c1678b53b2f3a014e
Karol Wasik <wasik.karol99@gmail.com>
2021-01-09 15:53:59 +0100

    add player by code

--- Form1.cs
+++ Form1.cs
@@ -34,28 +34,15 @@ namespace GwiezdnaFlota
             GenTim.Tick += new EventHandler(TimTickGenerateObjects);
             GenTim.Interval = 2000;
             GenTim.Start();
-
-            for(int i = 0; i < 30; i++)
-            {
-                
-                var enemy = new Enemy(100, 100);
-
-              //  Allies.Add(ally);
-                Enemies.Add(enemy);
-                //GamePanel.Controls.Add(ally);
-                //GamePanel.Controls.Add(enemy);
-            }
         }
 
         private void TimTickGenerateObjects(object sender, EventArgs e)
         {
-            if (Allies.Count == 3) GenTim.Stop();
-
             var ally = new Ally(r.Next(300,600), r.Next(300, 600));
             Allies.Add(ally);
             GamePanel.Controls.Add(ally);
-            //var npc = Allies[r.Next(0, 29)];
-            //GamePanel.Controls.Add(npc);
+
+            if (Allies.Count == 3) GenTim.Stop();
         }
 
         //menu controls//
@@ -63,8 +50,12 @@ namespace GwiezdnaFlota
         {
             GameStatus.game = false;
             GameStatus.ResetPoints();
+            GamePanel.Controls.Clear();
             Allies.Clear();
             GenTim.Start();
+
+            Player player = new Player(30, 453);
+            GamePanel.Controls.Add(player);
         }
 
         private void pb_Click(object sender, EventArgs e)
@@ -84,17 +75,8 @@ namespace GwiezdnaFlota
         {
             GameStatus.NextLevel();
             GameStatus.ResetPoints();
-            //Game();
-
-            //var picture = new Ally(300, 200);
-            //var picture2 = new Ally(400, 400);
-            //var picture3 = new Ally(200, 200);
-            //Allies.Add(picture);
-            //Allies.Add(picture2);
-            //Allies.Add(picture3);
-
-            //GamePanel.Controls.Add(picture);
-            //GamePanel.Controls.Add(picture2);
+            GamePanel.Controls.Clear();
+            Allies.Clear();
         }
         //menu controls//
 
@@ -134,29 +116,6 @@ namespace GwiezdnaFlota
             else
                 shooting = false;
         }
-
         //mouse movement and shooting//  
-        
-        public void ClearLists()
-        {
-            foreach(Ally al in Allies)
-            {
-                Allies.Remove(al);
-            }
-        }
-
-        public void Game()
-        {
-            while (GameStatus.game)
-            {
-                if (GameStatus.level == 1)
-                {
-                    foreach(Ally al in Allies)
-                    {
-                        GamePanel.Controls.Add(al);
-                    }
-                }
-            }
-        }
     }
 }
--- Models/Player.cs
+++ Models/Player.cs
@@ -4,19 +4,25 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace GwiezdnaFlota.Models
 {
-    class Player
+    public class Player : PictureBox
     {
         public int x;
         public int y;
-        public Image img;
-        public Player(int x, int y, Image img)
+       // public Image img;
+        public Player(int x, int y) //30,453
         {
             this.x = x;
             this.y = y;
-            this.img = img;
+            Name = "player";
+            Location = new Point(x, y);
+            Image = Image.FromFile(@"C:\Users\karol\Desktop\C#\GwiezdnaFlota\Galactic-Fleet\Img\playertrans.png");
+            Size = new Size(64, 64);
+            SizeMode = PictureBoxSizeMode.StretchImage;
+            BackColor = Color.Transparent;
         }
     }
 }
